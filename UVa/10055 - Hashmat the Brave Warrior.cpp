d7b9d4c87019136febf9c5c8113c99cfbfb1b105
diadia <99194548+diadia0000@users.noreply.github.com>
2023-12-12 13:22:34 +0800

    Delete UVa directory

--- UVa/10055 - Hashmat the Brave Warrior.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    long long int a,b;
-    while(cin>>a>>b){
-        cout<<max(a,b)-min(a,b)<<"\n";
-    }
-    return 0;
-}
--- UVa/10931 - Parity.py
+++
@@ -1,8 +0,0 @@
-import sys
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    Bin = str(bin(n))
-    count_one = Bin.count("1")
-    print(f"The parity of {Bin[2:]} is {count_one} (mod 2).")
